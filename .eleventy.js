45c52e6034b2fe8e314054dcd7100bead1a5eb15
Kevin Poston <kevposton@gmail.com>
2020-12-05 16:30:05 -0500

    adding books

--- .eleventy.js
+++ .eleventy.js
@@ -7,4 +7,21 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("readableDate", dateObj => {
     return dateObj.toDateString();
   });
+
+  eleventyConfig.addFilter("favoriteBooks", books => {
+    const favorites = books.filter(b => b.rank != undefined);
+    return favorites.sort((a, b) => a.rank - b.rank);
+  });
+
+  eleventyConfig.addFilter("currentlyReading", books => {
+    return books.filter(b => b.inProgress);
+  });
+
+  eleventyConfig.addFilter("groupedYears", books => {
+    const allYears = books.map(d => d.year);
+    const years = [...new Set(allYears)].sort((a, b) => b - a); 
+    const groups = years.map(year => ({year, books: books.filter(b => b.year === year)}));
+    
+    return groups;
+  });
 };
